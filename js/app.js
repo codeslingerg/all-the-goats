38589b167f0ce4dc570ab8bef3569d229a8dcddd
Gerard <gerard.g.johnson@gmail.com>
2023-06-03 17:49:07 -0500

    added images to page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+console.log("hello world");
 // TODOs: Make a Voting app for goat
 
 //TODO: global variables for elements
@@ -13,25 +14,42 @@ let resultsArea = document.getElementById("results-area");
 
 // Goat Constructor
 
-function Goat() {
-  //Got has
-  //    an image
-  //.   a name
-  //. vote count
-  //. view count
+function Goat(name, imgSrc) {
   this.name = this.name; // image
-  this.imgSrc = imgSrc; // a name
+  this.imgSrc = this.imgSrc; // a name
   this.voteCount = 0; //vote count
   this.viewCount = 0; // view count
 }
 
+//Declaring variables
 let cruisinGoat = new Goat("CruisinGoat", "./img/cruisin-goat.jpg");
 let floatGoat = new Goat("FloatYourGoat", "./img/float-your-goat.jpg");
 
+let goat1Img = document.getElementById("goat1");
+let goat2Img = document.getElementById("goat2");
+
 // Global
 let goatArray = [];
-
 goatArray.push(cruisinGoat);
 goatArray.push(floatGoat);
 
+console.log(goatArray);
+
 //TODO: Voting Machine DOM
+
+//input: goat objects
+//it takes in goat objects and puts them into the dom
+function setGoatImages(goat1, goat2) {
+  goat1Img.src = goat1.imgSrc;
+  goat1Img.alt = goat1.name;
+  goat1Img.title = goat1.name;
+  goat2Img.src = goat2.imgSrc;
+  goat2Img.alt = goat2.name;
+  goat2Img.title = goat2.name;
+}
+//TODO: Voting machine JS
+
+// goat1Img.src = goatArray[0].imgSrc;
+// goat2Img.src = goatArray[1].imgSrc;
+
+function(cruisinGoat, floatGoat);
